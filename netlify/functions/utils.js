325716966c962b944178dfb625b82642181af845
nikolastojadinov <nikolastojadinov@yahoo.co.uk>
2025-08-13 12:18:55 +0200

    Update utils.js

--- netlify/functions/utils.js
+++ netlify/functions/utils.js
@@ -1,16 +1,22 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+// netlify/functions/utils.js
 
+// CORS helper
 const cors = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'Content-Type, Authorization',
   'Access-Control-Allow-Methods': 'GET,POST,OPTIONS'
 };
 
-function ok(body, status=200){
+function ok(body, status = 200) {
   return { statusCode: status, headers: cors, body: JSON.stringify(body) };
 }
-function err(message, status=400){
+function err(message, status = 400) {
   return { statusCode: status, headers: cors, body: JSON.stringify({ error: message }) };
 }
 
-module.exports = { fetch, ok, err, cors };
+// Koristi natívni fetch iz Node 18+ (Netlify Functions runtime)
+async function fetch_(...args) {
+  return await fetch(...args);
+}
+
+module.exports = { fetch: fetch_, ok, err, cors };
